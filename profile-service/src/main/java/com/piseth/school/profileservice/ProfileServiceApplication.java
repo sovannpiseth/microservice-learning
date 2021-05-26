d3e35e1ea401e32d38ecd2d4208d707f0006fa28
Piseth Ing <piseth.ing@allianz.com>
2021-05-26 20:04:41 +0700

    add entity scan

--- profile-service/src/main/java/com/piseth/school/profileservice/ProfileServiceApplication.java
+++ profile-service/src/main/java/com/piseth/school/profileservice/ProfileServiceApplication.java
@@ -2,7 +2,9 @@ package com.piseth.school.profileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = "com.piseth.school.rentcloudcommon.model")
 @SpringBootApplication
 public class ProfileServiceApplication {
 
