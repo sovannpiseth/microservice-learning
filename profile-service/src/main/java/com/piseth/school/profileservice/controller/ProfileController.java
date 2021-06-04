e313954959f75db6c957491db2b38d0754af8c77
Ing Piseth <p.ing@gl-f.com>
2021-06-04 22:23:08 +0700

    add request mapping

--- profile-service/src/main/java/com/piseth/school/profileservice/controller/ProfileController.java
+++ profile-service/src/main/java/com/piseth/school/profileservice/controller/ProfileController.java
@@ -3,6 +3,7 @@ package com.piseth.school.profileservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.piseth.school.profileservice.service.CustomerService;
@@ -13,7 +14,7 @@ import com.piseth.school.rentcloudcommon.model.Customer;
 public class ProfileController {
 	@Autowired
 	CustomerService customerService;
-
+@RequestMapping(value = "profile", method = RequestMethod.POST)
 	public Customer save(@RequestBody Customer customer) {
 
 		return customerService.save(customer);
